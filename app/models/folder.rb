6a07dab24c5bdeb669e9ab6598536ee5c7f6e0ba
Brad West <brad@bradonomics.com>
2022-02-22 14:13:49 -0600

    Set folder permalink before saving

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,13 @@
 class Folder < ActiveRecord::Base
+  before_save :set_permalink
+
   has_many :articles
   validates :name, presence: true
+
+  private
+
+    def set_permalink
+      self.permalink = name.parameterize
+    end
+
 end
